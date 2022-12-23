86b49db9f2f552542a7ed73b8bd5c03e94cf248b
Warden <samer.charaf3@gmail.com>
2022-12-23 23:11:54 +0300

    Fixed Camera Movement

---
+++ Assets/Scripts/Camera/FollowTarget.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothTime;
+    private Vector3 offset;
+    private Vector3 currentVelocity = Vector3.zero;
+
+    private void Awake()
+    {
+        offset = transform.position - target.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = target.position + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime);
+    }
+}
